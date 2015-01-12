1f623469f2307a6dc8d6ea97028cf4a7896c4123
abhay-joshi-git <abhy.joshi@gmail.com>
2015-01-12 12:49:10 +0530

    communication between controllers using a service

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+//communication in nested controllers through events using $broadcast, $emit and $on
 angular.module('shoppingApp', [])
   .controller('shoppingMasterController', ['$scope', function($scope){
     $scope.balance = 1000;
@@ -26,3 +28,35 @@ angular.module('shoppingApp', [])
     });
 
   }]);
+
+angular.module('factoryERPApp', [])
+  .controller('productionFloorController', ['$scope', 'ironService', function($scope, ironService){
+    $scope.fetchIron = function(){
+      ironService.fetchIron(100);
+    };
+    $scope.$on('ironQtyChanged', function(event, args){
+      $scope.notEnoughIron = args.ironQty < 100 ;
+    });
+  }])
+  .controller('inventoryController', ['$scope', 'ironService', function($scope, ironService){
+    $scope.$on('ironQtyChanged', function(event, args){
+      $scope.ironAvailable = args.ironQty;
+    });
+    $scope.ironAvailable = ironService.getIronQty();
+  }])
+  .factory('ironService', function($rootScope){
+    var ironQty = 300;
+    var fetchIron = function(qty){
+      ironQty = ironQty - qty;
+      $rootScope.$broadcast('ironQtyChanged', {ironQty: ironQty});
+    };
+    var getIronAvailableQty = function(){
+      return ironQty;
+    };
+    return {
+      fetchIron: fetchIron,
+      getIronQty: getIronAvailableQty
+    }
+  });
+
+angular.bootstrap(angular.element('#ironFactoryERPApp'), ['factoryERPApp']);
